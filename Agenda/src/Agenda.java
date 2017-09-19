29583dc8096ec8fd9fbb92e7dd21fe8acf9cdff9
Heindell <hudsonrangel1@gmail.com>
2017-09-19 20:13:29 -0300

    Add files via upload

--- Agenda/src/Agenda.java
+++ Agenda/src/Agenda.java
@@ -1,4 +1,21 @@
+import java.util.Arrays;
 
 public class Agenda {
+	
+	private Contato[] contato;
+	private int quantidade;
+	
+	private int ultimoIndice;
+	public boolean inserir (Contato c){
+		Arrays.sort(contato);
+		if( Arrays.binarySearch(contato, c)>0){
+			return false;
+		}
+		if (contato.length==quantidade){
+			return false;
+		}
+		contato[quantidade++] = c;
+		return true;
+	}
 
 }
--- Agenda/src/Contato.java
+++ Agenda/src/Contato.java
@@ -1,4 +1,25 @@
 
-public class Contato {
+public class Contato implements Comparable {
 
+	@Override
+	public int compareTo(Object obj) {
+		Contato c = (Contato) obj;
+		return this.nome.compareTo(c.nome);
+		
+		
+	}
+	
+	public String nome;
+	public int telefone;
+	public String dataNascimento;
+	
+	public Contato(String nome, int telefone, String dataNascimento) {
+		super();
+		this.nome = nome;
+		this.telefone = telefone;
+		this.dataNascimento = dataNascimento;
+	}
+
+	
+	
 }
